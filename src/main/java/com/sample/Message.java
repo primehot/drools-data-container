efc7d509e494e0e2deb5ca194894ad26ca63093f
Oleh Tsyupa <oleh.tsyupa@vubiquity.com>
2018-09-10 14:54:14 +0200

    Init

---
+++ src/main/java/com/sample/Message.java
@@ -0,0 +1,35 @@
+package com.sample;
+
+/**
+ * created by OTsyupa
+ */
+public class Message {
+
+    public static final int HELLO = 0;
+    public static final int GOODBYE = 1;
+
+    private String message;
+
+    private int status;
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return " NEWToString()";
+    }
+}
